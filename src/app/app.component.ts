a02d987c9ad0127e89a9d3adeae7f4bf65f56320
Bielik20 <bielik20@gmail.com>
2017-02-13 14:30:05 +0100

    Routing: renaming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,18 @@
-import { Component } from '@angular/core';
-import {OnInit} from '@angular/core';
-import { Hero } from './hero'
-import { HeroService } from './hero.service'
+import { Component, OnInit } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent implements OnInit {
+
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.getHeroes();
+  ngOnInit() {
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
 }
